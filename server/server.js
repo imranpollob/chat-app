be9e56364cc55d7a02f2091152019722a9a6b9cc
pollmix <polboy777@gmail.com>
2018-12-18 11:12:28 +0600

    greet user and notify others

--- server/server.js
+++ server/server.js
@@ -14,14 +14,32 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (msg) => {
     console.log('createMessage', msg);
 
     io.emit('newMessage', {
-      to: "tanji",
-      text: "I am already here",
-      msg
+      from: msg.from,
+      text: msg.text,
+      createdAt: new Date().getTime()
     })
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
