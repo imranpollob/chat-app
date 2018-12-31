7275e0017e758046926d44caa37c576caf5ae121
pollmix <polboy777@gmail.com>
2018-12-31 15:25:53 +0600

    auto scrolling added

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,26 @@
 const socket = io()
 
+function scrollToBottom () {
+  let messages = jQuery('#messages')
+  let newMessage = messages.children('li:last-child')
+
+  let clientHeight = messages.prop('clientHeight')
+  let scrollTop = messages.prop('scrollTop')
+  let scrollHeight = messages.prop('scrollHeight')
+  let newMessageHeight = newMessage.innerHeight()
+  let lastMessageHeight = newMessage.prev().innerHeight()
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function () {
-  console.log('Connected to the server');
+  console.log('Connected to the server')
 })
 
 socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+  console.log('Disconnected from server')
 })
 
 socket.on('newMessage', function (message) {
@@ -18,6 +33,7 @@ socket.on('newMessage', function (message) {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 socket.on('newLocationMessage', function (message) {
@@ -30,6 +46,7 @@ socket.on('newLocationMessage', function (message) {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 jQuery('#message-form').on('submit', function (e) {
