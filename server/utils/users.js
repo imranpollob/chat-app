04a8809c2a611b0805df093ef24972da31dd0cbe
pollmix <polboy777@gmail.com>
2021-12-28 00:11:26 -0500

    added close chat button

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,14 +1,3 @@
-[{
-  id: '/#12poiajdspfoif',
-  name: 'Andrew',
-  room: 'The Office Fans'
-}]
-
-// addUser(id, name, room)
-// removeUser(id)
-// getUser(id)
-// getUserList(room)
-
 class Users {
   constructor () {
     this.users = [];
